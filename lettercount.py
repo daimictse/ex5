452c7f16b27a32c94eecd4591be886b46b6bb4bb
Hackbright Student <user@chromebox06.(none)>
2013-09-27 12:28:42 -0700

    Second ver with Christian's changes

--- lettercount.py
+++ lettercount.py
@@ -1,20 +1,24 @@
 from sys import argv
 
-script, text_file = argv
+def main():
+    script, text_file = argv
 
-text = open(text_file)
-text_read = text.read()
+    text = open(text_file)
+    text_read = text.read()
+    text.close()
 
-text_read = text_read.lower()
-list_of_letter = [0] * 26
+    text_read = text_read.lower()
+    list_of_letter = [0] * 26
 
-for char in text_read:
-    # only tracks alphabets a to z
-    if (ord(char) in range(97, 123)):
-        list_of_letter[ord(char)-97] += 1
+    for char in text_read:
+        char_idx = ord(char) - 97
+        # only tracks alphabets a to z
+        if char_idx >= 0 and char_idx < 26:
+            list_of_letter[char_idx] += 1
+        # if (ord(char) in range(97, 123)):
+            # list_of_letter[ord(char)-97] += 1
 
+    for item in list_of_letter:
+        print item
 
-for item in list_of_letter:
-    print item
-
-text.close()
+main()
